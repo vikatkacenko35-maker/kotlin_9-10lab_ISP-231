8332e1475a68cccef3fa7a2c3146bfd020d4ff47
vikatkacenko35-maker <vikatkacenko35@gmail.com>
2026-02-04 14:37:30 +0300

    refactor the errors

--- src/example/GameHero.kt
+++ src/example/GameHero.kt
@@ -2,7 +2,6 @@ package example
 
 import kotlin.properties.Delegates
 
-class UserProfile(initialName: String,  initialEmail: String){}
 class GameHero(startName: String){
     var name: String by Delegates.observable(startName){ _, old, new -> println("[$old] получил новое имя: $new!")}
     var mana: Int by Delegates.observable(100){_, old, new -> println("Мана:$old -> $new")}
